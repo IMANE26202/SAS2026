79bc6d0801c896f2d61fcc93a82c6508332c3492
IMANE <imanebouchrit18@gmail.com>
2026-09-21 11:41:48 +0100

    adding day1 challenges

---
+++ day1/exercice7.js
@@ -0,0 +1,15 @@
+/*Prime numbers*/
+
+for (let number = 2; number <= 50; number++) {
+    let isPrime = true;
+    for (let i = 2; i*i<= number; i++){
+        if (number % i === 0 ){
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(number);
+    }
+}   
+
---
+++ day1/exercice8.js
@@ -0,0 +1,9 @@
+/*Rhigt-Angled Triangle */
+const height = 5
+for (let i = 1; i <= height; i++) {
+    let ligne = " " ;
+    for (let j = 1; j <= i; j++) {
+        ligne += "*" ;
+    }
+    console.log(ligne) ;
+}      
